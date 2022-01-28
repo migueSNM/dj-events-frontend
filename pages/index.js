175a28e769f82357dea7a0548fbd431f0d52f7ca
Miguel Chirinos <miguel.chirinos.contractor@macmillan.com>
2022-01-28 13:50:44 -0300

    validate events

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function HomePage({events}) {
     <Layout>
       <h1>Upcoming Events</h1>
       {events.length === 0 && <h3>No events to show</h3>}
-      {events.map(evt => (
+      {events.length > 0 && events.map(evt => (
         <EventItem key={evt.id} evt={evt}/>
       ))}
       {events.length > 0 && (
